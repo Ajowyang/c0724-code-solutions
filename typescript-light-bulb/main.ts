36d032559e3b4effa8cc1e03bd65f63fd9fcaa27
ajowyang <andrewowyang875@gmail.com>
2024-07-23 22:43:48 +0000

    completed typescript-light-bulb -Andrew Owyang

---
+++ typescript-light-bulb/main.ts
@@ -0,0 +1,18 @@
+let on: boolean = false;
+
+const $container = document.querySelector('.container');
+if (!$container) throw new Error('.container query failed!');
+const $lightbulb = document.querySelector('.lightbulb');
+if (!$lightbulb) throw new Error('.lightbulb query failed!');
+
+$container.addEventListener('click', () => {
+  if (on === false) {
+    on = true;
+    $lightbulb.className = 'lightbulb light-on';
+    $container.className = 'container background-on';
+  } else {
+    on = false;
+    $lightbulb.className = 'lightbulb light-off';
+    $container.className = 'container background-off';
+  }
+});
