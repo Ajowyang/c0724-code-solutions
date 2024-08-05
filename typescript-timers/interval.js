c5826f0140a2fdd38e19e13d9f94a641c99bd2b5
ajowyang <andrewowyang875@gmail.com>
2024-08-05 18:21:09 +0000

    answered quiz questions, created variable in interval.ts to keep track of countdown display text

--- typescript-timers/interval.js
+++ typescript-timers/interval.js
@@ -1,13 +1,12 @@
 'use strict';
 const $intervalDisplay = document.querySelector('.countdown-display');
+let displayValue = $intervalDisplay.textContent;
 const intervalId = setInterval(function () {
-  if ($intervalDisplay.textContent) {
-    let currNum = parseInt($intervalDisplay.textContent) - 1;
-    if (currNum > 0) {
-      $intervalDisplay.textContent = currNum.toString();
-    } else {
-      $intervalDisplay.textContent = '~Earth Beeeelooowww Us~';
-      clearInterval(intervalId);
-    }
+  if (parseInt(displayValue) && parseInt(displayValue) >= 2) {
+    displayValue = (parseInt(displayValue) - 1).toString();
+    $intervalDisplay.textContent = displayValue;
+  } else {
+    $intervalDisplay.textContent = '~Earth Beeeelooowww Us~';
+    clearInterval(intervalId);
   }
 }, 1000);
--- typescript-timers/interval.ts
+++ typescript-timers/interval.ts
@@ -2,14 +2,14 @@ const $intervalDisplay = document.querySelector(
   '.countdown-display'
 ) as HTMLHeadingElement;
 
+let displayValue = $intervalDisplay.textContent as string;
+
 const intervalId = setInterval(function () {
-  if ($intervalDisplay.textContent) {
-    const currNum = parseInt($intervalDisplay.textContent) - 1;
-    if (currNum > 0) {
-      $intervalDisplay.textContent = currNum.toString();
-    } else {
-      $intervalDisplay.textContent = '~Earth Beeeelooowww Us~';
-      clearInterval(intervalId);
-    }
+  if (parseInt(displayValue) && parseInt(displayValue) >= 2) {
+    displayValue = (parseInt(displayValue) - 1).toString();
+    $intervalDisplay.textContent = displayValue;
+  } else {
+    $intervalDisplay.textContent = '~Earth Beeeelooowww Us~';
+    clearInterval(intervalId);
   }
 }, 1000);
