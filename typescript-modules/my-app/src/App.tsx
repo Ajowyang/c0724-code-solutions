c2a1f1db95318a8f23a05d3682d3e242adc39798
ajowyang <andrewowyang875@gmail.com>
2024-08-26 22:08:17 +0000

    completed typescript modules exercise

---
+++ typescript-modules/my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Header } from './Header';
+import { Footer } from './Footer';
+import { Body } from './Body';
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+}
